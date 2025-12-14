b29c269e70ff76be7c1298932a07ff9ccefcf988
albertzaw <albertzaw2208@gmail.com>
2025-12-15 02:05:44 +0800

    Implement Bank and Main classes

--- src/main/java/com/albert/bankManagementSys/Bank.java
+++ src/main/java/com/albert/bankManagementSys/Bank.java
@@ -1,1 +1,35 @@
 package com.albert.bankManagementSys;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bank{
+    private List<Account> accounts = new ArrayList<>();
+
+    public void addAccount(Account account){
+        accounts.add(account);
+        System.out.println("Account added successfully!");
+    }
+
+    public Account findAccount(int accountNum){
+        for (Account acc : accounts){
+            if (acc.getAccountNum() == accountNum){
+                return acc;
+            }
+        }
+        System.out.println("Account not found!");
+        return null;
+    }
+
+    public void transferFund(int fromAccNum, int toAccNum, double amount){
+        Account fromAcc = findAccount(fromAccNum);
+        Account toAcc = findAccount(toAccNum);
+        if (fromAcc != null && toAcc != null){
+            fromAcc.withdraw(amount);
+            toAcc.deposit(amount);
+            System.out.println("Transferred $" + amount + " from " + fromAccNum + " to " + toAccNum + " successfully." );
+        }
+    }
+
+
+}
--- src/main/java/com/albert/bankManagementSys/Main.java
+++ src/main/java/com/albert/bankManagementSys/Main.java
@@ -1,2 +1,20 @@
 package com.albert.bankManagementSys;
 
+public class Main{
+    public static void main(String[] args){
+        Bank bank = new Bank();
+
+        Account savings = new SavingsAccount(123, "Alice", 0, 5);
+        Account current = new CurrentAccount(456, "Bob", 200, 2000);
+
+        bank.addAccount(savings);
+        bank.addAccount(current);
+
+        savings.deposit(1000);
+        current.withdraw(100);
+
+        bank.transferFund(123, 456, 500);
+        System.out.println(savings);
+        System.out.println(current);
+    }
+}
